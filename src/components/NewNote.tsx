c87c3caf3788b9fb1660529016b1a8a02bd27aad
roob1y <robwork122@gmail.com>
2023-01-25 15:45:18 +0000

    Created skeleton for new note form

---
+++ src/components/NewNote.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NoteForm from "./NoteForm"
+
+export default function NewNote() {
+  return (
+    <>
+      <h1 className="mb-4">New</h1>
+      <NoteForm />
+    </>
+  );
+}
