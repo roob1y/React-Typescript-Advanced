c29b66ff6700f56bd8365abb73a1b1460c9f036f
roob1y <robwork122@gmail.com>
2023-01-25 20:42:23 +0000

    creates local storage and note creation

--- src/components/NewNote.tsx
+++ src/components/NewNote.tsx
@@ -1,11 +1,25 @@
-import React from "react";
-import NoteForm from "./NoteForm"
+import { NoteData, Tag } from "../App";
+import NoteForm from "./NoteForm";
 
-export default function NewNote() {
+type NewNoteProps = {
+  onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
+};
+
+export default function NewNote({
+  onSubmit,
+  onAddTag,
+  availableTags,
+}: NewNoteProps) {
   return (
     <>
       <h1 className="mb-4">New</h1>
-      <NoteForm />
+      <NoteForm
+        onSubmit={onSubmit}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
+      />
     </>
   );
 }
